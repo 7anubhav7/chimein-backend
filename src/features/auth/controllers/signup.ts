a1330ebccb9ae6b00e51eb093cd82735e17621b5
anubhav <anubhavhajela37@gmail.com>
2025-07-22 22:59:08 +0530

    Sign up route established

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -9,11 +9,13 @@ import { BadRequestError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
 import { UploadApiResponse } from 'cloudinary';
 import { upload } from '@global/helpers/cloudinary-upload';
+import HTTP_STATUS from 'http-status-codes';
 
 export class SignUp {
   @joiValidation(signupSchema)
   public async create(req: Request, res: Response): Promise<void> {
-    const { username, password, email, avatarColor, avatarImage } = req.body;
+    console.log('Received', req.body);
+    const { username, email, password, avatarColor, avatarImage } = req.body;
     const checkIfUserExists: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
     if (checkIfUserExists) {
       throw new BadRequestError('Invalid credentials');
@@ -21,7 +23,7 @@ export class SignUp {
     const authObjectId: ObjectId = new ObjectId();
     const userObjectId: ObjectId = new ObjectId();
     const uId = `${Helpers.generateRandomIntegers(12)}`;
-    const authData: IAuthDocument = this.signUpData({
+    const authData: IAuthDocument = SignUp.prototype.signupData({
       _id: authObjectId,
       uId,
       username,
@@ -29,13 +31,16 @@ export class SignUp {
       password,
       avatarColor
     });
+    console.log('Data gotten from signupDat', authData);
     const result: UploadApiResponse = (await upload(avatarImage, `${userObjectId}`, true, true)) as UploadApiResponse;
+    console.log(result);
     if (!result?.public_id) {
       throw new BadRequestError('File upload: Error occurred. Try again');
     }
+    res.status(HTTP_STATUS.CREATED).json({ message: 'User created successfully' });
   }
 
-  private signUpData(data: ISignUpData): IAuthDocument {
+  private signupData(data: ISignUpData): IAuthDocument {
     const { _id, username, email, uId, password, avatarColor } = data;
     return {
       _id,
