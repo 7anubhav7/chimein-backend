0274e46a1a290344a632c869f14707b9dfea048b
anubhav <anubhavhajela37@gmail.com>
2025-10-31 13:51:09 +0530

    Fixed after install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,7 +4,7 @@ cd /home/ec2-user/chimein-backend
 sudo rm -rf env-file.zip
 sudo rm -rf .env
 sudo rm -rf .env.develop
-aws s3 sync s3://chimein-bucket/develop .
+aws s3 sync s3://chimein-bucket/backend/develop .
 unzip env-file.zip
 sudo cp .env.develop .env
 sudo npm run delete
